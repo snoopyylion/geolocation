b51557cd0847f11a255f0dcc67dcea599bdc45ac
snoopyylion <tifase_oluwaseyi@yahoo.com>
2024-03-20 14:31:39 +0100

    the icon didnt work so i removed it

--- location/src/App.jsx
+++ location/src/App.jsx
@@ -1,61 +1,59 @@
 import './App.css';
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer, TileLayer, Marker, Popup  } from "react-leaflet";
-import { Icon, divIcon } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {  divIcon } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 
 function App() {
   // markers
   const markers = [
     {
-      geocode: [9.0820, 8.6753],
-      popUp: "Hello, i am pop up 1"
+      geocode: [6.6194, 3.5105],
+      popUp: "Hello, I am pop up 1"
     },
     {
-      geocode: [9.0815, 8.6750],
-      popUp: "Hello, i am pop up 2"
+      geocode: [6.5244, 3.3792],
+      popUp: "Hello, I am pop up 2"
     },
     {
-      geocode: [9.0810, 8.6745],
-      popUp: "Hello, i am pop up 3"
+      geocode: [7.3775, 3.9470],
+      popUp: "Hello, I am pop up 3"
     }
   ];
 
-  const customIcon = new Icon({
-    iconUrl: require("./assets/location.png"),
-    iconSize:[38, 38] // size of the icon
-  });
 
   const createCustomClusterIcon = (cluster) => {
-    return new divIcon({
-      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`
+    return divIcon({
+      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
       className: "custom-marker-cluster",
-      iconSize: point(33, 33, true) 
-    })
-  }
-
+    });
+  };
 
   return (
     <>
-  <MapContainer center={[9.0820,8.6753]} zoom={13}>
-      <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <MapContainer center={[6.6194, 3.5105]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-    <MarkerClusterGroup
-    chunkedLoading
-    iconCreateFunction={createCustomClusterIcon}
-    >
-      {markers.map((marker) => (
-        <Marker position={marker.geocode} icon={customIcon}>
-          <Popup>{marker.popUp}</Popup>
-        </Marker>
-        ))}
-    </MarkerClusterGroup>
-  </MapContainer> 
+        <MarkerClusterGroup
+          chunkedLoading
+          iconCreateFunction={createCustomClusterIcon}
+        >
+          {markers.map((marker) => (
+            <Marker
+              key={marker.geocode.toString()}
+              position={marker.geocode}
+            >
+              <Popup>{marker.popUp}</Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+      </MapContainer>
     </>
-   );
-} 
-export default App
+  );
+}
+
+export default App;
